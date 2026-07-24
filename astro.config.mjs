776e30a0fe6d7d286cfd4fcb8a99b09ccfef6ac0
parker <parker@parkerbritt.com>
2026-07-24 09:09:17 +0100

    refactor: transition to astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-	prefetch: {
-		prefetchAll: true,
+  prefetch: {
+      prefetchAll: true,
 	},
-	redirects: {
-		"/docs": "/docs/guide/install-build",
-		"/docs/guide": "/docs/guide/install-build",
+
+  redirects: {
+      "/docs": "/docs/guide/install-build",
+      "/docs/guide": "/docs/guide/install-build",
 	},
-	markdown: {
-		shikiConfig: {
-			theme: "github-dark-dimmed",
-		},
+
+  markdown: {
+      shikiConfig: {
+          theme: "github-dark-dimmed",
+      },
 	},
+
+  integrations: [icon()],
 });
